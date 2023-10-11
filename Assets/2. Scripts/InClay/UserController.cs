27ada6090b2dde80db1169aff30449aadcea9147
kodending <82422156+kodending@users.noreply.github.com>
2023-10-11 16:01:18 +0900

    클레이사격 추가
    클레이사격 코드 및 에셋 추가

---
+++ Assets/2. Scripts/InClay/UserController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserController : MonoBehaviour
+{
+    [SerializeField] private FixedJoystick fixJoy;  // 조이스틱 호출
+
+    private float fRotX;        // X축 회전
+    private float fRotY;        // Y축 회전
+    public float fRotSpeed;     // 회전 속도
+
+    private void Awake()
+    {
+        fRotX = fRotY = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        JoyControl();
+    }
+
+    private void JoyControl()
+    {
+        //회전값에 제한을 줘야된다
+        fRotX -= fixJoy.Vertical * fRotSpeed;
+        fRotY += fixJoy.Horizontal * fRotSpeed;
+
+        transform.rotation = Quaternion.Euler(fRotX, fRotY, 0);
+    }
+}
